e34fda3041ecdb1a18d310857e28aa97b4f759ee
Mallarpu Deepak sai <mdeepaksai806@gmail.com>
2026-06-29 13:24:22 +0530

    Optimize maximum average subarray calculation
    Implemented an optimized sliding window approach for calculating the maximum average of a subarray of size k, replacing the brute force method.

--- arrays/sliding window/643-maximum-average-subarray-i/maximum-average-subarray-i.java
+++ arrays/sliding window/643-maximum-average-subarray-i/maximum-average-subarray-i.java
@@ -1,3 +1,28 @@
+/////brute force/////
+
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        int n = nums.length;
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i <= n - k; i++) {
+            int sum = 0;
+
+            for (int j = i; j < i + k; j++) {
+                sum += nums[j];
+            }
+
+            max = Math.max(max, sum);
+        }
+
+        return (double) max / k;
+    }
+}
+
+
+////optimized code////
+///sliding window///
+
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
         int n=nums.length;
