350cda9181309a78a770a3a6242d67f9441feeae
Mallarpu Deepak sai <mdeepaksai806@gmail.com>
2026-06-29 15:28:43 +0530

    Implement two-pointer method for sorting colors
    Added an alternative two-pointer approach for sorting colors.

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,3 +1,5 @@
+////optimized way/////
+
 class Solution {
     public void sortColors(int[] nums) {
         int zero=0;
@@ -29,6 +31,36 @@ class Solution {
             nums[i]=2;
             i++;
             two--;
+        }
+    }
+}
+
+/////another way ////
+////using two pointers////
+
+class Solution {
+    public void sortColors(int[] nums) {
+        int n = nums.length;
+        int low=0;
+        int high=n-1;
+        int mid=0;
+        while(mid<=high){
+            if(nums[mid]==0){
+                int temp=nums[mid];
+                nums[mid]=nums[low];
+                nums[low]=temp;
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else{
+                int temp=nums[mid];
+                nums[mid]=nums[high];
+                nums[high]=temp;
+                high--;
+            }
         }
     }
 }
