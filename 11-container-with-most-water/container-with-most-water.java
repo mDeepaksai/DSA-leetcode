8d6fc62754de355366ff9f2710dd24e042bfc292
Mallarpu Deepak sai <mdeepaksai806@gmail.com>
2026-06-30 11:14:21 +0530

    Update container-with-most-water.java

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -1,3 +1,30 @@
+////brute force/////
+class Solution {
+    public int maxArea(int[] height) {
+        int n = height.length;
+        int max = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+
+                int h = Math.min(height[i], height[j]);
+                int width = j - i;
+
+                int area = h * width;
+
+                max = Math.max(area, max);
+            }
+        }
+
+        return max;
+    }
+}
+
+
+////optimized way////
+////using two pointer////
+
+
 class Solution {
     public int maxArea(int[] height) {
         int n = height.length;
