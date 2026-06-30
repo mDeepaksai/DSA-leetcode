90b5d63f259a58b413949f25f68ec2660e3ba7be
Mallarpu Deepak sai <mdeepaksai806@gmail.com>
2026-06-30 09:57:12 +0530

    Time: 6 ms (23.05%) | Memory: 45.8 MB (86.86%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,31 +1,3 @@
-////brute force////
-
-class Solution {
-    public boolean isPalindrome(int x) {
-
-        String str = Integer.toString(x);
-        String pal = "";
-
-        for (int i = str.length() - 1; i >= 0; i--) {
-            pal += str.charAt(i);
-        }
-
-        for (int i = 0; i < str.length(); i++) {
-            for (int j = i; j < pal.length(); j++) {
-                if (str.charAt(i) != pal.charAt(j)) {
-                    return false;
-                }
-                break;
-            }
-        }
-
-        return true;
-    }
-}
-
-/////optiimized way/////
-////two pointers////
-
 class Solution {
     public boolean isPalindrome(int x) {
 
