e79fd2a932d76c0812d58c8b1bc545e5f7c8e78c
Mallarpu Deepak sai <mdeepaksai806@gmail.com>
2026-06-30 08:49:51 +0530

    Update two-sum-ii-input-array-is-sorted.java

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -1,3 +1,5 @@
+////brute force/////
+
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
         int n=numbers.length;
@@ -8,6 +10,32 @@ class Solution {
                 return new int[]{i+1, j+1};
                 }
             }
+        }
+        return new int[]{-1,-1};
+
+    }
+}
+
+/////optimized way//////
+/////using two pointers/////
+
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int n=numbers.length;
+        int left=0;
+        int right=n-1;
+        while(left<right){
+            int sum=numbers[left]+numbers[right];
+            if(sum==target){
+                return new int[]{left+1,right+1};
+            }
+            else if(sum>target){
+                right--;
+            }
+            else{
+                left++;
+            }
+
         }
         return new int[]{-1,-1};
 
