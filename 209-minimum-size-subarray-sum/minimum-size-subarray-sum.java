c783fb028fc1143061add8db571bf8417180151a
Mallarpu Deepak sai <mdeepaksai806@gmail.com>
2026-06-30 21:58:04 +0530

    Implement brute force method for min subarray sum
    Added a brute force method to calculate the minimum size subarray sum in addition to the optimized sliding window approach.

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
@@ -1,3 +1,6 @@
+////optimized/////
+////this sliding window //////
+
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
         int ans = Integer.MAX_VALUE;
@@ -13,6 +16,32 @@ class Solution {
             }
         }
         if(ans==Integer.MAX_VALUE){
+            return 0;
+        }
+        return ans;
+    }
+}
+
+/////brute force/////
+
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int ans = Integer.MAX_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+            int sum = 0;
+
+            for (int j = i; j < nums.length; j++) {
+                sum += nums[j];
+
+                if (sum >= target) {
+                    ans = Math.min(ans, j - i + 1);
+                    break;
+                }
+            }
+        }
+
+        if (ans == Integer.MAX_VALUE) {
             return 0;
         }
         return ans;
