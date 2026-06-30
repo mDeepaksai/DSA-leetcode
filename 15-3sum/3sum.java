f0f2585223b7eeebb577f6b704b668c6d65a57a4
Mallarpu Deepak sai <mdeepaksai806@gmail.com>
2026-06-30 11:07:27 +0530

    Update 3sum.java

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,3 +1,42 @@
+/////brute force/////
+
+
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+
+        Set<List<Integer>> set = new HashSet<>();
+
+        int n = nums.length;
+
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1; j < n - 1; j++) {
+                for (int k = j + 1; k < n; k++) {
+
+                    int sum = nums[i] + nums[j] + nums[k];
+
+                    if (sum == 0) {
+
+                        List<Integer> triplet =
+                                Arrays.asList(nums[i], nums[j], nums[k]);
+
+                        Collections.sort(triplet);
+
+                        set.add(triplet);
+                    }
+                }
+            }
+        }
+
+        return new ArrayList<>(set);
+    }
+}
+
+
+////optimized way////
+////using two pointer/////
+
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
