81bc1f306a92706b10d43bd946cddf475fe5afb5
Mallarpu Deepak sai <mdeepaksai806@gmail.com>
2026-06-30 09:23:11 +0530

    Update move-zeroes.java

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,3 +1,25 @@
+////brute force////
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+         int n=nums.length;
+         int[] arr=new int[n];
+         int ind=0;
+         for(int i=0;i<n;i++){
+            if(nums[i]!=0){
+                arr[ind]+=nums[i];
+                ind++;
+            }
+         }
+         for(int i=0;i<n;i++){
+            nums[i]=arr[i];
+         }
+    }
+}
+
+/////optimized way/////
+////using two pointers////
+
 class Solution {
     public void moveZeroes(int[] nums) {
          int n=nums.length;
