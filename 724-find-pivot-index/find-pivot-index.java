6197a1e6ada68048b56c2a7cd69d48c8cebf8c9b
Mallarpu Deepak sai <mdeepaksai806@gmail.com>
2026-07-05 18:05:25 +0530

    Implement brute force and optimized pivot index methods
    Added brute force and optimized methods for finding pivot index.

--- 724-find-pivot-index/find-pivot-index.java
+++ 724-find-pivot-index/find-pivot-index.java
@@ -1,3 +1,29 @@
+/////brute force////
+
+
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            int leftsum=0;
+            for(int j=0;j<i;j++){
+                leftsum+=nums[j];
+            }
+            int rightsum=0;
+            for(int j=i+1;j<n;j++){
+                rightsum+=nums[j];
+            }
+            if(rightsum==leftsum){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+
+/////optimized way/////
+
+
 class Solution {
     public int pivotIndex(int[] nums) {
         int n=nums.length;
