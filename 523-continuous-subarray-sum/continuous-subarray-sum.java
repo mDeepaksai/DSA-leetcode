505c7d1cf98a06df12dfd9ca1c87362b72b0f71e
Mallarpu Deepak sai <mdeepaksai806@gmail.com>
2026-07-06 20:59:10 +0530

    Optimize checkSubarraySum with hashmap and prefix sum
    Refactored checkSubarraySum method to use optimal approach with hashmap and prefix sum.

--- 523-continuous-subarray-sum/continuous-subarray-sum.java
+++ 523-continuous-subarray-sum/continuous-subarray-sum.java
@@ -1,3 +1,26 @@
+////brute force////
+
+class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            int sum=0;
+            for(int j=i;j<n;j++){
+                sum+=nums[j];
+                if(sum%k==0 && j-i+1>=2){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
+
+
+///optimal way////
+/////using hashmap + prefixsum//////
+
+
 class Solution {
     public boolean checkSubarraySum(int[] nums, int k) {
         HashMap<Integer,Integer> map=new HashMap<>();
