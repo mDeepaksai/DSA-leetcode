940a196e287fbba0ee07667240cfeaca9807c66d
Mallarpu Deepak sai <mdeepaksai806@gmail.com>
2026-07-08 18:37:18 +0530

    Update maximum-product-subarray.java

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -1,3 +1,25 @@
+////brute force///
+
+
+class Solution {
+    public int maxProduct(int[] nums) {
+     int maxans=nums[0];
+     int n=nums.length;
+     for(int i=0;i<n;i++){
+        int prod=1;
+        for(int j=i;j<n;j++){
+            prod*=nums[j];
+            maxans=Math.max(prod,maxans);
+        }
+     }   
+     return maxans;
+    }
+}
+
+
+
+////optimized way////
+
 class Solution {
     public int maxProduct(int[] nums) {
      int n=nums.length;
