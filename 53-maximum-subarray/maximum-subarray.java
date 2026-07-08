b5b2f59995eb5b63ccead0012b58b76b08f9b757
Mallarpu Deepak sai <mdeepaksai806@gmail.com>
2026-07-08 17:33:57 +0530

    Optimize maxSubArray using Kadane's algorithm
    Refactor maxSubArray method to implement Kadane's algorithm for optimal performance.

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,3 +1,24 @@
+///brute force////
+
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int n=nums.length;
+        int max=nums[0];
+        for(int i=0;i<n;i++){
+            int sum=0;
+            for(int j=i;j<n;j++){
+                sum+=nums[j];
+                max=Math.max(max,sum);
+            }
+        }
+        return max;
+    }
+}
+
+////optimal way/////
+////using kadane's algorthim////
+
 class Solution {
     public int maxSubArray(int[] nums) {
         int n=nums.length;
