878a0195f665e2c3e0f0d38dce8467ff0f7ee34a
Mallarpu Deepak sai <mdeepaksai806@gmail.com>
2026-07-09 18:28:57 +0530

    Update contiguous-array.java

--- 525-contiguous-array/contiguous-array.java
+++ 525-contiguous-array/contiguous-array.java
@@ -1,3 +1,31 @@
+////brute force///
+
+
+class Solution {
+    public int findMaxLength(int[] nums) {
+        int n=nums.length;
+        int len=0;
+        for(int i=0;i<n;i++){
+        int count0=0;
+        int count1=0;
+            for(int j=i;j<n;j++){
+                if(nums[j]==1){
+                    count0++;
+                }
+                if(nums[j]==0){
+                    count1++;
+                }
+                if(count0==count1){
+                    len=Math.max(len,j-i+1);
+                }
+            }
+        }
+        return len;
+    }
+}
+
+////optimal way////
+
 class Solution {
     public int findMaxLength(int[] nums) {
         HashMap<Integer,Integer> map=new HashMap<>();
