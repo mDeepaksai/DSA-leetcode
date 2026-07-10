ae8dbe1371e4bc3840b89df77efdc8980067a83e
Mallarpu Deepak sai <mdeepaksai806@gmail.com>
2026-07-10 11:50:56 +0530

    Add comments to max-consecutive-ones.java
    Added comments to explain the sliding window approach for counting consecutive ones.

--- arrays/sliding window/dynamic window/485-max-consecutive-ones/max-consecutive-ones.java
+++ arrays/sliding window/dynamic window/485-max-consecutive-ones/max-consecutive-ones.java
@@ -1,3 +1,7 @@
+////sliding window////
+////first we will count all Consecutive ones if we get one in between of them then reset the count=0, then again start the count of one from 0 then return max count of consecutive ones in array////
+////simple problem only////
+
 class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
         int n=nums.length;
