406c1bc2829248ce716c2931f67c3c165ef30b80
Mallarpu Deepak sai <mdeepaksai806@gmail.com>
2026-07-11 15:57:53 +0530

    Time: 44 ms (27.32%) | Memory: 47.6 MB (18.52%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,19 +1,28 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         ArrayList<Integer> res=new ArrayList<>();
-        int n=s.length();
-        int k=p.length();
-        int[] pcount=new int[26];
+        HashMap<Character,Integer> pmap=new HashMap<>();
+        HashMap<Character,Integer> smap=new HashMap<>();
         for(char c:p.toCharArray()){
-            pcount[c-'a']++;
-        }   
-        for(int i=0;i<=n-k;i++){
-            int[] scount =new int[26];
-            for(int j=i;j<i+k;j++){
-                scount[s.charAt(j)-'a']++;
+            pmap.put(c,pmap.getOrDefault(c,0)+1);
+        }
+        int left=0, count=p.length();
+        for(int right=0;right<s.length();right++){
+            char ch=s.charAt(right);
+            smap.put(ch,smap.getOrDefault(ch,0)+1);
+            if(pmap.containsKey(ch) && smap.get(ch)<=pmap.get(ch)){
+                count--;
             }
-            if(Arrays.equals(pcount,scount)){
-                res.add(i);
+            if(right -left+1>p.length()){
+                char leftchar=s.charAt(left);
+                if(pmap.containsKey(leftchar) && smap.get(leftchar)<=pmap.get(leftchar)){
+                    count++;
+                }
+                smap.put(leftchar,smap.get(leftchar)-1);
+                left++;
+            }
+            if(count==0){
+                res.add(left);
             }
         }
         return res;
