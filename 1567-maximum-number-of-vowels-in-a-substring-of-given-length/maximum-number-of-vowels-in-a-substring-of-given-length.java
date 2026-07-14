edf7541d33404f7070a5e08478fb2ca6701c0ca3
Mallarpu Deepak sai <mdeepaksai806@gmail.com>
2026-07-14 09:33:58 +0530

    Update maximum-number-of-vowels-in-a-substring-of-given-length.java

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,3 +1,5 @@
+////simple sliding window////
+
 class Solution {
     public int maxVowels(String s, int k) {
         HashMap<Character, Integer> map = new HashMap<>();
@@ -18,5 +20,43 @@ class Solution {
 
     public boolean isvowel(char ch) {
         return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
+    }
+}
+
+////with hashmap and sliding window////
+
+
+class Solution {
+    public int maxVowels(String s, int k) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        int n = s.length();
+        int max = 0;
+        int count = 0; 
+        int left = 0;
+        
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+            
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                map.put(ch, map.getOrDefault(ch, 0) + 1);
+                count++;
+            }
+            
+            if (i - left + 1 > k) {
+                char cleft = s.charAt(left);
+                if (map.containsKey(cleft)) {
+                    map.put(cleft, map.get(cleft) - 1);
+                    if (map.get(cleft) == 0) {
+                        map.remove(cleft);
+                    }
+                    count--;
+                }
+                left++;
+            }
+            
+            max = Math.max(max, count);
+        }
+        
+        return max;
     }
 }
