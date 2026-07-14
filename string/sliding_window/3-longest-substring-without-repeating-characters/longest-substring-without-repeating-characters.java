9b5b55efe10576ad01fdf34b27b542be82dba804
Mallarpu Deepak sai <mdeepaksai806@gmail.com>
2026-07-14 17:55:23 +0530

    Update longest-substring-without-repeating-characters.java

--- string/sliding_window/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ string/sliding_window/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -44,3 +44,24 @@ class Solution {
         return maxlen;
     }
 }
+
+////using hashset////
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        HashSet<Character> set=new HashSet<>();
+        int n=s.length();
+        int maxlen=0;
+        int left=0;
+        for(int right=0;right<n;right++){
+            while(set.contains(s.charAt(right))){
+                set.remove(s.charAt(left));
+                left++;
+            }
+            set.add(s.charAt(right));
+            maxlen = Math.max(maxlen, right - left + 1);
+
+        }
+        return maxlen;
+    }
+}
