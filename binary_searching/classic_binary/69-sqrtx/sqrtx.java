d751602277c009f2e1e7cd07418b633045bdc9b5
Mallarpu Deepak sai <mdeepaksai806@gmail.com>
2026-07-19 16:33:18 +0530

    Update sqrtx.java

--- binary_searching/classic_binary/69-sqrtx/sqrtx.java
+++ binary_searching/classic_binary/69-sqrtx/sqrtx.java
@@ -1,3 +1,9 @@
+////optimal way///
+////using binary search////
+//// why we used binary search - because that num which we need to find sqrt we will represent it in number line and sqrt of that number is alway less x/2 of that number 
+//// if the sqrt is greater that x then end=mid-1 and if sqrt is lesser than x then start = mid+1
+//// and sqrt should be in long while u convert it into int simple
+
 class Solution {
     public int mySqrt(int x) {
         if(x<2) return x;
