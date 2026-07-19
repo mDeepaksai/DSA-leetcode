f0beb7fc65f018c385e6be0a63f2e0321e19b228
Mallarpu Deepak sai <mdeepaksai806@gmail.com>
2026-07-19 17:07:18 +0530

    Implement brute force and start binary search method
    Added a brute force approach and started implementing an optimal binary search method for finding the insert position.

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -1,3 +1,23 @@
+////brute force O(N)////
+
+
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>=target){
+                return i;
+            }
+        }
+        return nums.length;
+    }
+}
+
+
+////optimal way////
+//// at the end we are returning the left because when left and right pointer is pointing to same number at that time that number should be inserted at that position////
+
+
+
 class Solution {
     public int searchInsert(int[] nums, int target) {
         int left=0;
