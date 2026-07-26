c29dde08ee7d8c2df515ff24039348579a948740
Mallarpu Deepak sai <mdeepaksai806@gmail.com>
2026-07-26 20:53:18 +0530

    Update find-peak-element.java

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -1,8 +1,36 @@
+////using linear searching 
+//time comp-O(N)
+
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int n = nums.length;
+        
+        //before three statment is base cases
+        if (n == 1) return 0;
+        if (nums[0] > nums[1]) return 0;
+        if (nums[n - 1] > nums[n - 2]) return n - 1;
+
+        
+        //linear search 
+        for (int i = 1; i < n - 1; i++) {
+            if (nums[i] > nums[i + 1] && nums[i] > nums[i - 1]) {
+                return i;
+            }
+        }
+        
+        return 0;
+    }
+}
+
+
+////optimal code using binary search
+//time comp-O(log N)
+
 class Solution {
     public int findPeakElement(int[] nums) {
         int n = nums.length;
         int right = n - 1, left = 0;
-        while (left < right) {//
+        while (left < right) {//moving uoward or moving right side
             int mid = left + (right - left) / 2;
             if (nums[mid] > nums[mid + 1])
                 right = mid;
