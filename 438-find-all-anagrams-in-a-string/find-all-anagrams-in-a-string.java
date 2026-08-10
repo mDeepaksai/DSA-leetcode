979f70f52ffc01ea591a7873480db8e3ea17f29f
Mallarpu Deepak sai <mdeepaksai806@gmail.com>
2026-08-10 21:49:04 +0530

    Time: 44 ms (26.64%) | Memory: 47.7 MB (18.02%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -2,26 +2,55 @@ import java.util.*;
 
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
+
         List<Integer> res = new ArrayList<>();
+
         int n = s.length();
         int k = p.length();
 
-        if (n < k) return res;
+        if (n < k)
+            return res;
 
-        int[] pcount = new int[26];
+        Map<Character, Integer> pmap = new HashMap<>();
+        Map<Character, Integer> smap = new HashMap<>();
+
+        // Frequency of characters in p
         for (char c : p.toCharArray()) {
-            pcount[c - 'a']++;
+            pmap.put(c, pmap.getOrDefault(c, 0) + 1);
         }
 
-        for (int i = 0; i <= n - k; i++) {
-            int[] scount = new int[26];
+        int left = 0;
+        int count = k;
 
-            for (int j = i; j < i + k; j++) {
-                scount[s.charAt(j) - 'a']++; 
+        for (int right = 0; right < n; right++) {
+
+            char ch = s.charAt(right);
+
+            smap.put(ch, smap.getOrDefault(ch, 0) + 1);
+
+            // Character contributes to matching p
+            if (pmap.containsKey(ch) &&
+                smap.get(ch) <= pmap.get(ch)) {
+                count--;
             }
 
-            if (Arrays.equals(pcount, scount)) {
-                res.add(i);
+            // Window size becomes greater than k
+            if (right - left + 1 > k) {
+
+                char lchar = s.charAt(left);
+
+                if (pmap.containsKey(lchar) &&
+                    smap.get(lchar) <= pmap.get(lchar)) {
+                    count++;
+                }
+
+                smap.put(lchar, smap.get(lchar) - 1);
+                left++;
+            }
+
+            // All characters matched
+            if (count == 0) {
+                res.add(left);
             }
         }
 
