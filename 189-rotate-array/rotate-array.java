6e8cc3cf922852df13a2605f497c64975cd6df88
Mallarpu Deepak sai <mdeepaksai806@gmail.com>
2026-08-18 22:02:13 +0530

    Update rotate-array.java

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,3 +1,20 @@
+// brute force
+
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n=nums.length;
+        for(int i=0;i<k;i++){
+            int temp=nums[n-1];
+            for(int j=n-2;j>=0;j--){
+                nums[j+1]=nums[j];
+            }
+            nums[0]=temp;
+        }
+    }
+}
+
+// optimal code
+// what are we doing here is first creating the temp array then that that index we add that original array element and aftering completing it we will just copy the temp to nums that's all
 class Solution {
     public void rotate(int[] nums, int k) {
         int n=nums.length;
