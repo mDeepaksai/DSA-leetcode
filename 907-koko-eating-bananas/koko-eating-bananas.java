d82777837184ea95eff78355135aeddc2575b045
Mallarpu Deepak sai <mdeepaksai806@gmail.com>
2026-08-19 20:32:31 +0530

    Update koko-eating-bananas.java

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,3 +1,30 @@
+// brute force
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int low=1;
+        int high=0;
+        for(int hr:piles){
+            high=Math.max(hr,high);
+        }
+        while(low<=high){
+            int k=low+(high-low)/2;
+            int hours=0;
+            for(int p:piles){
+                hours+=(p+k-1)/k;
+            }
+            if(hours<=h){
+                high=k-1;
+            }
+            else{
+                low=k+1;
+            }
+        }
+        return low;
+    }
+}
+
+
+// optimal code
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
         int low=1;
