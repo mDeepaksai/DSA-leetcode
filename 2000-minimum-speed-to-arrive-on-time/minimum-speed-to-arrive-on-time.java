8370e0523a6e7d63c66278cca875e4e879d65162
Mallarpu Deepak sai <mdeepaksai806@gmail.com>
2026-08-27 17:17:28 +0530

    Update minimum-speed-to-arrive-on-time.java

--- 2000-minimum-speed-to-arrive-on-time/minimum-speed-to-arrive-on-time.java
+++ 2000-minimum-speed-to-arrive-on-time/minimum-speed-to-arrive-on-time.java
@@ -1,3 +1,33 @@
+// brute force
+
+
+class Solution {
+    public int minSpeedOnTime(int[] dist, double hour) {
+        int n=dist.length;
+        int low=1;
+        int high=10000000;
+        for(int speed=low;speed<=high;speed++){
+            double time=0.0;
+            for(int i=0;i<n;i++){
+                double t=(double) dist[i]/speed;
+                if(i!=n-1){
+                    time+=Math.ceil(t);
+                }
+                else{
+                    time+=t;
+                }
+            }
+            if(time<=hour){
+                return speed;
+            }
+        }
+        return -1;
+    }
+}
+
+// optimal code
+
+
 class Solution {
     public int minSpeedOnTime(int[] dist, double hour) {
         int low = 1;
