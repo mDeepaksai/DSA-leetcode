1b597561c92b7207bd5d1f0beae39c02451d88c4
Mallarpu Deepak sai <mdeepaksai806@gmail.com>
2026-08-27 16:51:02 +0530

    Time: 135 ms (43.63%) | Memory: 99.3 MB (61.71%) - LeetSync

---
+++ 2000-minimum-speed-to-arrive-on-time/minimum-speed-to-arrive-on-time.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int minSpeedOnTime(int[] dist, double hour) {
+        int low = 1;
+        int high = 10000000;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (canReach(dist, hour, mid)) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    private boolean canReach(int[] dist, double hour, int speed) {
+        int n = dist.length;
+        double time = 0.0;
+        for (int i = 0; i < n; i++) {
+            double t = (double) dist[i] / speed;
+            if (i != n - 1) {
+                time += Math.ceil(t);
+            } else {
+                time += t;
+            }
+        }
+        return time <= hour;
+    }
+}
