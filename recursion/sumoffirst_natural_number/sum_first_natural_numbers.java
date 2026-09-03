e836a328c7f47d33c6fd9fa04832344108e70ad6
mDeepaksai <mdeepaksai806@gmail.com>
2026-09-03 18:47:54 +0530

    basic

---
+++ recursion/sumoffirst_natural_number/sum_first_natural_numbers.java
@@ -0,0 +1,17 @@
+public class recursion {
+    public static void printSum(int i,int n,int sum){
+        if(i==n){
+            sum+=i;
+            System.out.println(sum);
+            return;
+        }
+        // System.out.println(n);
+        sum+=i;
+        printSum(i+1,n,sum);
+        System.out.println(i);
+    }
+    public static void main(String[] args) {
+        int n=5;
+        printSum(1,n,0);
+    }
+}
