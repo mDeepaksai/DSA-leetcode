ddbd3b313700ba336dc2f1a3ebe4ae90d7b6a93a
Mallarpu Deepak sai <mdeepaksai806@gmail.com>
2026-09-04 14:55:33 +0530

    Implement recursive climbStairs method
    Added a recursive solution for climbing stairs using O(2^N) approach.

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,3 +1,6 @@
+// O(N)
+// it is similar to fibonacci but it starts from 1 not 0 that's all 
+
 class Solution {
 
     public int climbStairs(int n) {
@@ -11,5 +14,16 @@ class Solution {
         }
 
         return climbCheck(b, a + b, n - 1);
+    }
+}
+
+// O(2**N)
+
+class Solution {
+    public int climbStairs(int n) {
+        if(n<=2){
+            return n;
+        }
+        return climbStairs(n-1)+climbStairs(n-2);
     }
 }
