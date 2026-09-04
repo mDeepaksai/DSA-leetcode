7c1db2b00664633a415e797c6c50eebd2519ea98
Mallarpu Deepak sai <mdeepaksai806@gmail.com>
2026-09-04 14:34:52 +0530

    Update fibonacci-number.java

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,3 +1,5 @@
+// O(N)
+
 class Solution {
 
     public int fibonacci(int a, int b, int n) {
@@ -12,5 +14,16 @@ class Solution {
     public int fib(int n) {
 
         return fibonacci(0, 1, n);
+    }
+}
+
+// O(2**N)
+
+class Solution {
+    public int fib(int n) {
+        if(n<=1){
+            return n;
+        }
+        return fib(n-1)+fib(n-2);
     }
 }
