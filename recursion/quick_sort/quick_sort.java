510873cb7ac8c4e42027e2ad0b290f41ee0d304a
mDeepaksai <mdeepaksai806@gmail.com>
2026-09-08 18:38:58 +0530

    quicksort

---
+++ recursion/quick_sort/quick_sort.java
@@ -0,0 +1,26 @@
+
+int partition(int* arr, int low, int high) {
+    int pivot=arr[high];
+    int i=low-1;
+    for(int j=low;j<high;j++){
+        if(arr[j]<pivot){
+            i++;
+            int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+        }
+    }
+    i++;
+    int temp=arr[i];
+    arr[i]=arr[high];
+    arr[high]=temp;
+    return i;
+}
+
+void quickSort(int* arr, int low, int high) {
+    if(low<high){
+        int pidx=partition(arr,low,high);
+        quickSort(arr,low,pidx-1);
+        quickSort(arr,pidx+1,high);
+    }
+}
