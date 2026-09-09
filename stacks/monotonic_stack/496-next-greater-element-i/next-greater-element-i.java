823723ecc27f624a87135b1add5a262b7047e425
mDeepaksai <mdeepaksai806@gmail.com>
2026-09-09 20:27:09 +0530

    update monostanic

--- stacks/monotonic_stack/496-next-greater-element-i/next-greater-element-i.java
+++ stacks/monotonic_stack/496-next-greater-element-i/next-greater-element-i.java
@@ -22,21 +22,35 @@ class Solution {
 
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int[] nextGreater = new int[10001];
-        Stack<Integer> stack = new Stack<>();
 
-        for (int i = nums2.length - 1; i >= 0; i--) {
-            while (!stack.isEmpty() && stack.peek() <= nums2[i]) {
-                stack.pop();
+        int n = nums2.length;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        Stack<Integer> s = new Stack<>();
+
+        for(int i = n - 1; i >= 0; i--){
+
+            while(!s.isEmpty() && s.peek() <= nums2[i]){
+                s.pop();
             }
-            nextGreater[nums2[i]] = stack.isEmpty() ? -1 : stack.peek();
-            stack.push(nums2[i]);
+
+            if(!s.isEmpty()){
+                map.put(nums2[i], s.peek());
+            }
+            else{
+                map.put(nums2[i], -1);
+            }
+
+            s.push(nums2[i]);
         }
 
-        for (int i = 0; i < nums1.length; i++) {
-            nums1[i] = nextGreater[nums1[i]];
+        int[] ans = new int[nums1.length];
+
+        for(int i = 0; i < nums1.length; i++){
+            ans[i] = map.get(nums1[i]);
         }
 
-        return nums1;
+        return ans;
     }
 }
