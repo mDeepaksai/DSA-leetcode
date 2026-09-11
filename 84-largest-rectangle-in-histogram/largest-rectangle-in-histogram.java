ed22f4dcb7a7c357069bda09cf0a2b2d05390b6a
Mallarpu Deepak sai <mdeepaksai806@gmail.com>
2026-09-11 18:23:25 +0530

    Optimize largestRectangleArea method
    Refactored the largestRectangleArea method to include an optimized approach using a stack for better performance.

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,3 +1,27 @@
+// brute force
+
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        int n=heights.length;
+        if(n==0){
+            return 0;
+        }
+        int max=0;
+        for(int i=0;i<n;i++){
+            int h=heights[i];
+            for(int j=i;j<n;j++){
+                h=Math.min(h,heights[j]);
+                int width=j-i+1;
+                int area=h*width;
+                max=Math.max(area,max);
+            }
+        }
+        return max;
+    }
+}
+
+// optimized code
+
 class Solution {
     public int largestRectangleArea(int[] heights) {
         int n=heights.length;
