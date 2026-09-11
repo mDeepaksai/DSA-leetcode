548593205a93701ee071ac0749eaee987acebd7f
Mallarpu Deepak sai <mdeepaksai806@gmail.com>
2026-09-11 19:11:14 +0530

    Time: 6 ms (19.49%) | Memory: 46.7 MB (89.71%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.java
+++ 735-asteroid-collision/asteroid-collision.java
@@ -1,36 +1,22 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-       List<Integer>list=new ArrayList<>();
-       for(int x:asteroids){
-        list.add(x);
-       } 
-       int i=0;
-       while(i<list.size()-1){
-        int a=list.get(i);
-        int b=list.get(i+1);
-        if(a>0 && b<0){
-            if(Math.abs(a)>Math.abs(b)){
-                list.remove(i+1);
-            }
-            else if(Math.abs(a)<Math.abs(b)){
-                list.remove(i);
-                if(i>0)
-                i-=1;
-            }
-            else{
-                list.remove(i+1);
-                list.remove(i);
-                if(i>0)
-                i-=1;
+       Stack<Integer>s=new Stack<>();
+       for(int a:asteroids){
+        while(!s.isEmpty() && a<0 && s.peek()>0 && s.peek()<-a){
+            s.pop();
+        }
+        if(!s.isEmpty() && a<0 && s.peek()>0){
+            if(s.peek()==-a){
+                s.pop();
             }
         }
         else{
-            i++;
+            s.push(a);
         }
        }
-       int[] ans=new int[list.size()];
-       for(i=0;i<list.size();i++){
-        ans[i]=list.get(i);
+       int[] ans=new int[s.size()];
+       for(int i=s.size()-1;i>=0;i--){
+        ans[i]=s.pop();
        }
        return ans;
     }
