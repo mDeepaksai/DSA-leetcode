e03bc8229743ae750693aca4ee97ae16dbf55bd2
Mallarpu Deepak sai <mdeepaksai806@gmail.com>
2026-09-11 20:14:34 +0530

    Optimize asteroid collision algorithm
    Refactored asteroid collision logic from brute force to optimized stack-based approach.

--- 735-asteroid-collision/asteroid-collision.java
+++ 735-asteroid-collision/asteroid-collision.java
@@ -1,3 +1,45 @@
+// brute force
+
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+       List<Integer>list=new ArrayList<>();
+       for(int x:asteroids){
+        list.add(x);
+       } 
+       int i=0;
+       while(i<list.size()-1){
+        int a=list.get(i);
+        int b=list.get(i+1);
+        if(a>0 && b<0){
+            if(Math.abs(a)>Math.abs(b)){
+                list.remove(i+1);
+            }
+            else if(Math.abs(a)<Math.abs(b)){
+                list.remove(i);
+                if(i>0)
+                i-=1;
+            }
+            else{
+                list.remove(i+1);
+                list.remove(i);
+                if(i>0)
+                i-=1;
+            }
+        }
+        else{
+            i++;
+        }
+       }
+       int[] ans=new int[list.size()];
+       for(i=0;i<list.size();i++){
+        ans[i]=list.get(i);
+       }
+       return ans;
+    }
+}
+
+// optimized code
+
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
        Stack<Integer>s=new Stack<>();
